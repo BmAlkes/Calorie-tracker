e3974495eea6cf5e661b6b735dc2e9cd251d8aa8
bmalkes <bmalkes@gmail.com>
2024-04-08 20:01:33 +0300

    feat:add local storage

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import logo from "./assets/fitness_2964514.png";
+import Form from "./components/Form";
+import { useEffect, useReducer } from "react";
+import { activityReducer, initialState } from "./reducers/activity-reducer";
+import ActivityList from "./components/ActivityList";
+
+function App() {
+  const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("activity", JSON.stringify(state.activities));
+  }, [state.activities]);
+
+  return (
+    <>
+      <header className="bg-lime-600 py-3">
+        <div className="max-w-4xl mx-auto flex justify-between items-center">
+          <div className="flex items-center">
+            <img src={logo} alt="logo" className="w-[100px]" />
+            <h1 className="text-center text-lg font-bold text-white uppercase">
+              Calories Tracker
+            </h1>
+          </div>
+
+          <button className="overflow-hidden  w-40 p-2 h-12 bg-slate-500 text-white border-none rounded-md text-xl font-bold cursor-pointer relative z-10 group">
+            Clear
+            <span className="absolute w-44 h-32 -top-8 -left-2 bg-green-200 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-bottom"></span>
+            <span className="absolute w-44 h-32 -top-8 -left-2 bg-green-400 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-bottom"></span>
+            <span className="absolute w-44 h-32 -top-8 -left-2 bg-green-600 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-bottom"></span>
+            <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">
+              Well Done 😄
+            </span>
+          </button>
+        </div>
+      </header>
+      <section className="bg-lime-500 py-20 px-5">
+        <div className="max-w-4xl mx-auto">
+          <Form dispatch={dispatch} state={state} />
+        </div>
+      </section>
+      <section className="p-10 mx-auto max-w-4xl">
+        <ActivityList activities={state.activities} dispatch={dispatch} />
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,109 @@
+import { ChangeEvent, Dispatch, FormEvent, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { categories } from "../data";
+import { Activity } from "../types";
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer";
+
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>;
+  state: ActivityState;
+};
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  activity: "",
+  calories: 0,
+};
+
+const Form = ({ dispatch, state }: FormProps) => {
+  const [saveActivity, setSaveActivity] = useState<Activity>(initialState);
+
+  useEffect(() => {
+    if (state.activeId) {
+      const selectedActivity = state.activities.filter(
+        (stateActivity) => stateActivity.id === state.activeId
+      )[0];
+      setSaveActivity(selectedActivity);
+    }
+  }, [state.activeId]);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
+
+    setSaveActivity({
+      ...saveActivity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
+    });
+  };
+  const isValidAcvity = () => {
+    const { activity, calories } = saveActivity;
+    return activity.trim() !== " " && calories > 0;
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "save-activity", payload: { newActivity: saveActivity } });
+    setSaveActivity({ ...initialState, id: uuidv4() });
+  };
+  return (
+    <form
+      className="space-y-5 bg-white p-10 rounded-lg shadow-2xl"
+      onSubmit={handleSubmit}
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="" className="font-bold">
+          Categories
+        </label>
+        <select
+          name="category"
+          id="category"
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          value={saveActivity.category}
+          onChange={handleChange}
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">
+          Activity
+        </label>
+        <input
+          type="text"
+          id="activity"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Example: food, orange juice, salad, exercises, "
+          value={saveActivity.activity}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className="font-bold">
+          Calories
+        </label>
+        <input
+          type="number"
+          id="calories"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Example: calories , 200 or 500 "
+          value={saveActivity.calories}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-600 w-full p-2 font-bold text-white uppercase cursor-pointer disabled:opacity-10"
+        value={saveActivity.category === 1 ? "Save Food" : "Save Exercise"}
+        disabled={!isValidAcvity()}
+      />
+    </form>
+  );
+};
+
+export default Form;
